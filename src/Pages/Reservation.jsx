9b79b3a101f1a6da3af706b0d3a8c069898d8ead
Evedbs <evedubuisson5@gmail.com>
2024-08-12 16:44:51 +0200

    added BookingForm component as a child of the Reservation page.

--- src/Pages/Reservation.jsx
+++ src/Pages/Reservation.jsx
@@ -7,6 +7,118 @@ import ChipList from "../Components/ChipList.jsx";
 import { useState } from "react";
 import ConfirmationReservation from "./ConfirmationReservation.jsx";
 
+function BookingForm({ value, setValue, handleSubmit }) {
+  return (
+    <section className="ReservationPage">
+      <img
+        src={restaurant}
+        className="imgReservation"
+      />
+      <Typography
+        gutterBottom
+        variant="h5"
+        component="div"
+        sx={{
+          color: "#495e57",
+          fontFamily: "Markazi-text",
+          fontSize: 56,
+          marginLeft: "20%",
+          fontStyle: "italic",
+          marginTop: "96px",
+        }}
+      >
+        Book Now
+      </Typography>
+      <form>
+        <div className="containerInputs">
+          <TextField
+            className="input"
+            required
+            id="outlined-required"
+            label="First name"
+            placeholder="John"
+            value={value.firstName}
+            onChange={(e) => setValue({ ...value, firstName: e.target.value })}
+          />
+          <TextField
+            className="input"
+            required
+            id="outlined-required"
+            label="Last name"
+            placeholder="Doe"
+            value={value.lastName}
+            onChange={(e) => setValue({ ...value, lastName: e.target.value })}
+          />
+          <TextField
+            className="input"
+            required
+            type="email"
+            id="outlined-required"
+            label="Email"
+            placeholder="JohnDoe@gmail.com"
+            value={value.email}
+            onChange={(e) => setValue({ ...value, email: e.target.value })}
+          />
+          <TextField
+            className="input"
+            required
+            id="outlined-required"
+            label="Phone number"
+            placeholder="06 00 00 00 00"
+            value={value.phoneNumber}
+            onChange={(e) =>
+              setValue({ ...value, phoneNumber: e.target.value })
+            }
+          />
+          <div className="dateTime">
+            <DateTime
+              value={value}
+              setValue={setValue}
+            />
+          </div>
+          <div className="guestCount">
+            <GuestCount
+              value={value}
+              setValue={setValue}
+            />
+          </div>
+          <div className="containerChips">
+            <div className="chip">
+              <ChipList
+                title="Occasion"
+                value={["None", "Engagement", "Anniversary"]}
+              />
+            </div>
+            <div
+              className="chip"
+              style={{ marginTop: 32 }}
+            >
+              <ChipList
+                title="Seating preferences"
+                value={["No preferences", "Indoor", "Outdoor", "Veranda"]}
+              />
+            </div>
+          </div>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{
+              fontFamily: "karla",
+              fontSize: 20,
+              marginTop: "32px",
+              marginBottom: "96px",
+            }}
+            onClick={handleSubmit}
+            size="large"
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
 export default function Reservation() {
   const [value, setValue] = useState({
     firstName: "",
@@ -18,7 +130,6 @@ export default function Reservation() {
   });
 
   const [toggle, setToggle] = useState(false);
-
   const handleSubmit = () => {
     if (
       value.firstName === "" ||
@@ -39,117 +150,11 @@ export default function Reservation() {
       {toggle ? (
         <ConfirmationReservation value={value} />
       ) : (
-        <section className="ReservationPage">
-          <img
-            src={restaurant}
-            className="imgReservation"
-          />
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{
-              color: "#495e57",
-              fontFamily: "Markazi-text",
-              fontSize: 56,
-              marginLeft: "20%",
-              fontStyle: "italic",
-              marginTop: "96px",
-            }}
-          >
-            Book Now
-          </Typography>
-          <form>
-            <div className="containerInputs">
-              <TextField
-                className="input"
-                required
-                id="outlined-required"
-                label="First name"
-                placeholder="John"
-                value={value.firstName}
-                onChange={(e) =>
-                  setValue({ ...value, firstName: e.target.value })
-                }
-              />
-              <TextField
-                className="input"
-                required
-                id="outlined-required"
-                label="Last name"
-                placeholder="Doe"
-                value={value.lastName}
-                onChange={(e) =>
-                  setValue({ ...value, lastName: e.target.value })
-                }
-              />
-              <TextField
-                className="input"
-                required
-                type="email"
-                id="outlined-required"
-                label="Email"
-                placeholder="JohnDoe@gmail.com"
-                value={value.email}
-                onChange={(e) => setValue({ ...value, email: e.target.value })}
-              />
-              <TextField
-                className="input"
-                required
-                id="outlined-required"
-                label="Phone number"
-                placeholder="06 00 00 00 00"
-                value={value.phoneNumber}
-                onChange={(e) =>
-                  setValue({ ...value, phoneNumber: e.target.value })
-                }
-              />
-              <div className="dateTime">
-                <DateTime
-                  value={value}
-                  setValue={setValue}
-                />
-              </div>
-              <div className="guestCount">
-                <GuestCount
-                  value={value}
-                  setValue={setValue}
-                />
-              </div>
-              <div className="containerChips">
-                <div className="chip">
-                  <ChipList
-                    title="Occasion"
-                    value={["None", "Engagement", "Anniversary"]}
-                  />
-                </div>
-                <div
-                  className="chip"
-                  style={{ marginTop: 32 }}
-                >
-                  <ChipList
-                    title="Seating preferences"
-                    value={["No preferences", "Indoor", "Outdoor", "Veranda"]}
-                  />
-                </div>
-              </div>
-              <Button
-                variant="contained"
-                color="secondary"
-                sx={{
-                  fontFamily: "karla",
-                  fontSize: 20,
-                  marginTop: "32px",
-                  marginBottom: "96px",
-                }}
-                onClick={handleSubmit}
-                size="large"
-              >
-                Submit
-              </Button>
-            </div>
-          </form>
-        </section>
+        <BookingForm
+          value={value}
+          setValue={setValue}
+          handleSubmit={handleSubmit}
+        />
       )}
     </>
   );
